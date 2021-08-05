033f40046718fde205f0208af038176ef4bc9404
suyizZZ <695708197@qq.com>
2021-08-05 16:37:12 +0800

    修改打包配置homepage publicPath

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,25 @@
 const CracoLessPlugin = require('craco-less');
+const path = require('path');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, {
+      env, paths
+    }) => {
+        // paths.appPath='public'
+        paths.appBuild = 'dist';
+        webpackConfig.output = {
+          ...webpackConfig.output,
+            // ...{
+          //   filename: whenDev(() => 'static/js/bundle.js', 'static/js/[name].js'),
+          //   chunkFilename: 'static/js/[name].js'
+          // },
+          path: path.resolve(__dirname, 'dist'), // 修改输出文件目录
+          publicPath: '/canvas-filter/',
+        }
+        return webpackConfig;
+    }
+  },
   // ...
   plugins: [
     {
